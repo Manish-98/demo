6652dca2c5354c9ddbf7cb9c534cafbbc5ef2265
Manish Kumar Lath <manishlath3445@gmail.com>
2025-01-17 12:52:32 +0530

    Commit 2

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    private Integer id;
+    private String name;
+}
